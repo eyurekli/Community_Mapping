a29201692f1132199e8b02d4fc5bf5ab41d89d69
yurekli <kurumizawa38@gmail.com>
2024-10-05 14:09:00 -0300

    Updated Landing Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,20 @@
 import './App.css'
 import LandingPage from './components/LandingPage'
 import Earth from './components/Earth'
+import { useState } from 'react';
 
 function App() {
+
+  const [displayMain, setDisplayMain] = useState(true);
+
+  const toggleDisplay = () => {
+    setDisplayMain((prevDisplay) => !prevDisplay); // Toggles the state
+  };
 
   return (
     
     <div className="">
-      <Earth/>
+      {displayMain ? <LandingPage onToggleDisplay={toggleDisplay}/> : <Earth/>}
     </div>
   )
 }
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,10 +1,26 @@
+import "../css/LandingPage.css"
 
-
-function LandingPage() {
+function LandingPage({ onToggleDisplay }) {
     
     return (
         <div className="lanPage">
-            <p>Naber</p>
+            <header>
+                <h2>WF Map</h2>
+                <nav>
+                    <ul>
+                        <li>Home</li>
+                        <li>Services</li>
+                        <li>Product</li>
+                        <li>About Us</li>
+                    </ul>
+                </nav>
+            </header>
+
+            <section className="info">
+                <h1>Community Wildfire Map</h1>
+                <p>Stay informed about wildfires around the world</p>
+                <button onClick={onToggleDisplay}>Try Now</button>
+            </section>           
         </div>
     )
 }
